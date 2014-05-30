aff5e67dc246e9eb9d68e490003a1a5a3f4034e0
Don DeCaprio <github@decaprio.com>
2014-05-29 23:03:20 -0700

    fix migration

--- db/migrate/20140530054647_add_drug_name_to_drugs.rb
+++ db/migrate/20140530054647_add_drug_name_to_drugs.rb
@@ -1,6 +1,6 @@
 class AddDrugNameToDrugs < ActiveRecord::Migration
   def change
     add_column :drugs, :drug_name, :string
-    remove_column :drugs, :name, :text
+    remove_column :drugs, :name
   end
 end
